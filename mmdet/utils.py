f2bc09faffd91ee9ef174ca2ddb7f01a131502db
HibernationNo1 <winter4958@gmail.com>
2023-06-27 15:00:59 +0900

    feat: delete old load_ext

--- mmdet/utils.py
+++ mmdet/utils.py
@@ -50,18 +50,6 @@ def to_tensor(data):
     
 
 
-def load_ext(name, funcs):
-    # TODO: 
-    # 1. 해당 package를 pypi에 올린 후 pip install한다.
-    #    필. _ext.cp38-win_amd64.pyd 또는 linux용 C소스 module을 포함하여 upload.
-    # 2. 아래 "mmcv."을 f"{upload한 module_name}으로 대체" 
-    ext = importlib.import_module("mmcv." + name)   
-    for fun in funcs:
-        assert hasattr(ext, fun), f'{fun} miss in module {name}'
-    
-    return ext
-
-
 
 # TODO: using  
 def auto_scale_lr(cfg, logger, num_gpus = 1):   
