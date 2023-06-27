2ccc87f426111c2a6207f3eab898f3333b266b71
HibernationNo1 <winter4958@gmail.com>
2023-06-27 11:52:24 +0900

    feat: delete import from mmdet

--- mmdet/modules/register_module.py
+++ mmdet/modules/register_module.py
@@ -1,30 +1,12 @@
 from sub_module.mmdet.registry import Registry
 
 
+BACKBONES = Registry('backbone')
 
-from mmdet.models.backbones.swin import SwinTransformer as _SwinTransformer
-BACKBORN = Registry('backbone')
-@BACKBORN.register_module()
-def SwinTransformer(**cfg):
-    return _SwinTransformer(**cfg)
+NECKS = Registry('neck')
 
+RPN_HEADS = Registry('rpn_head')
 
-from mmdet.models.necks.fpn import FPN as _FPN
-NECK = Registry('neck')
-@NECK.register_module()
-def FPN(**cfg):
-    return _FPN(**cfg)
+ROI_HEADS = Registry('roi_head')
 
-from mmdet.models.dense_heads.rpn_head import RPNHead as _RPNHead 
-RPN_HEAD = Registry('rpn_head')
-@RPN_HEAD.register_module()
-def RPNHead(**cfg):
-    return _RPNHead(**cfg)
-
-
-from mmdet.models.roi_heads.standard_roi_head import StandardRoIHead as _StandardRoIHead
-ROI_HEAD = Registry('roi_head')
-@ROI_HEAD.register_module()
-def StandardRoIHead(**cfg):
-    return _StandardRoIHead(**cfg)
 
